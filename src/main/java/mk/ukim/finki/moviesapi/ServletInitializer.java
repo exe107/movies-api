35e9d50a5a436d0266df0fe242e281c835542c1d
exe <ex177@hotmail.com>
2019-12-22 17:13:39 +0100

    Initialize project
    - add required dependencies
    - temporarily comment persistence related dependencies

---
+++ src/main/java/mk/ukim/finki/moviesapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.moviesapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MoviesApiApplication.class);
+	}
+
+}
